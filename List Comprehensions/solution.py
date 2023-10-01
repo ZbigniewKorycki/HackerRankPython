09f0eef651cf3964b266a589d7512c8fd1489808
Zbigniew Korycki <zbigniewkorycki@gmail.com>
2023-10-01 11:58:47 +0200

    add List Comprehensions exercise

---
+++ List Comprehensions/solution.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = int(input("x: "))
+    y = int(input("y: "))
+    z = int(input("z: "))
+    n = int(input("n: "))
+
+    permutation = [[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n]
+    print(permutation)
