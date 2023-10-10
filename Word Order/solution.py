a8d7dfb4798ac056fddaa58eea44e23a060b9408
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-10 16:24:57 +0200

    add Word Order - final solution

--- Word Order/solution.py
+++ Word Order/solution.py
@@ -2,18 +2,16 @@ num_words = int(input())
 
 words = [input() for _ in range(num_words)]
 
-print(len(set(words)))
-
-words_counter = []
+words_with_occurrence = {}
 for word in words:
-    word_occurrence = words.count(word)
-    words_counter.append(word_occurrence)
-    words = list(filter(word.__ne__, words))
+    if word in words_with_occurrence:
+        words_with_occurrence[word] += 1
+    else:
+        words_with_occurrence[word] = 1
 
-zero = 0
-words_counter_without_zeros = list(filter(zero.__ne__,words_counter))
+words_values = [str(occurrence) for occurrence in words_with_occurrence.values()]
 
-final_words = [str(n) for n in words_counter_without_zeros]
+print(len(words_with_occurrence))
+print(' '.join(words_values))
 
-print(' '.join(final_words))
 
