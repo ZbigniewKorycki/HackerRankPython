717d98640aa9990abd67c427025a57be3122ae1d
Zbigniew Korycki <zbigniewkorycki@gmail.com>
2023-10-21 12:28:33 +0200

    add Triangle Quest 2 solution max points

--- Triangle Quest 2/solution.py
+++ Triangle Quest 2/solution.py
@@ -1,2 +1,2 @@
 for i in range(1, int(input())+1):
-    print(sum(d * 10**n for n,d in enumerate([*range(1, i)] + [*range(i,0, -1)])))
+    print(int(123456789/(10**(9-i)))*(10**(i-1))+int(987654321%(10**(i-1))))
