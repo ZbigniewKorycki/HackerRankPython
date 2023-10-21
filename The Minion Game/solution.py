db8f96e1092fb7c0b1cdecd98f3065fdc73fcb2d
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-10-21 18:53:13 +0200

    add The Minion Game finished exercise

--- The Minion Game/solution.py
+++ The Minion Game/solution.py
@@ -1,27 +1,19 @@
-import time
-
 def minion_game(string):
     points_kevin = 0
-    score_stuart = 0
-    string_len = len(string)
-    for word_len in range(1, string_len+1):
-        for letter_index in range(string_len - word_len + 1):
-            word = string[letter_index:letter_index+word_len]
-            if word[0] in "AEIOU":
-                points_kevin += 1
-            else:
-                score_stuart += 1
+    points_stuart = 0
 
-    if points_kevin < score_stuart:
-        print("Stuart", score_stuart)
-    elif points_kevin > score_stuart:
+    for index, letter in enumerate(string.upper(), 0):
+        if letter in "AEIOU":
+            points_kevin += len(string) - index
+        else:
+            points_stuart += len(string) - index
+    if points_kevin < points_stuart:
+        print("Stuart", points_stuart)
+    elif points_kevin > points_stuart:
         print("Kevin", points_kevin)
     else:
         print("Draw")
 
 if __name__ == '__main__':
-    s = "W" * 10000
-    start = time.perf_counter()
+    s = input()
     minion_game(s)
-    end = time.perf_counter()
-    print(end-start)
