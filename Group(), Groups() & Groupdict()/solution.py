63a9ad507dd3089b588f4b48070c6b1cf1284c1f
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-02 13:59:32 +0100

    add solution to exercise Group(), Groups() & Groupdict() - using match

--- Group(), Groups() & Groupdict()/solution.py
+++ Group(), Groups() & Groupdict()/solution.py
@@ -2,9 +2,11 @@ import re
 
 S = input()
 
-m = re.findall(r'([a-zA-Z0-9])\1', S)
-
-if m:
-    print(m[0])
-else:
-    print(-1)
+for letter in range(len(S)):
+    word = S[letter:]
+    m = re.match(r'([a-zA-Z0-9])\1', word)
+    if m:
+        print(S[letter])
+        break
+    elif letter == len(S) - 1:
+        print(-1)
