e98b8e1cfbf4fad9b0ebe104cc4bffc9a858437c
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-06 18:27:48 +0100

    add working solution, pass all tests

--- Detect HTML Tags, Attributes and Attribute Values/solution.py
+++ Detect HTML Tags, Attributes and Attribute Values/solution.py
@@ -1,32 +1,17 @@
 import re
 
-html_code = [input() for _ in range(int(input()))]
+def find_match(html_code):
+    matches = re.findall('<([a-zA-Z0-9]+)>*|\s([a-zA-Z0-9/-]+)="([^"]+)"', html_code)
+    if matches:
+        for match in matches:
+            if match[0]:
+                print(match[0])
+            elif match[1] and match[2]:
+                print(f"-> {match[1]} > {match[2]}")
 
+
+html_code = [input() for _ in range(int(input()))]
 html_code_joined = ''.join(html_code)
 
-if "<!--" in html_code_joined and "-->" in html_code_joined:
-    flag = True
-else:
-    flag = False
-
-while flag:
-    code_split = html_code_joined.split("<!--")
-    code_before_comment = code_split[0]
-    code_with_comment = code_split[1]
-    try:
-        code_after_comment = code_with_comment.split("-->")[1]
-    except IndexError:
-        code_after_comment = ''
-    else:
-        html_code_joined = code_before_comment + code_after_comment
-    if "<!--" not in html_code_joined and "-->" not in html_code_joined:
-        flag = False
-
-
-matches = re.findall('<([a-zA-Z0-9]+)>*|\s([a-zA-Z0-9/-]+)="([^"]+)"', html_code_joined)
-if matches:
-    for match in matches:
-        if match[0]:
-            print(match[0])
-        elif match[1] and match[2]:
-            print(f"-> {match[1]} > {match[2]}")
+text_without_comments = re.sub(r'<!--.*?-->', '', html_code_joined)
+find_match(text_without_comments)
