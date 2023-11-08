f13fb92a82e9e41ad49a311c335f34b947ed1fa5
ZbigniewKorycki <Zbigniewkorycki.info@gmail.com>
2023-11-08 12:08:47 +0100

    add solution to exercise Decorators 2 - Name Directory

--- Decorators 2 - Name Directory/solution.py
+++ Decorators 2 - Name Directory/solution.py
@@ -2,7 +2,11 @@ import operator
 
 def person_lister(f):
     def inner(people):
-        # complete the function
+        for person in people:
+            person[2] = int(person[2])
+        people.sort(key=operator.itemgetter(2))
+        people_with_name_format = [f(person) for person in people]
+        return people_with_name_format
     return inner
 
 @person_lister
